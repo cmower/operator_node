963b2d889062f32e5a5201c613ccccac1a092f7d
Chris E. Mower <chris.mower@ed.ac.uk>
2021-08-25 15:42:48 +0100

    Minor code formatting

--- src/operator_node/parser.py
+++ src/operator_node/parser.py
@@ -1,7 +1,9 @@
 import numpy
 from scipy.interpolate import interp1d
 
+
 class ParseInterfaceLog:
+
 
     # https://docs.scipy.org/doc/scipy/reference/generated/scipy.interpolate.interp1d.html
     interpolate_config = dict(
@@ -10,14 +12,17 @@ class ParseInterfaceLog:
         fill_value='extrapolate'
     )
 
+
     def __init__(self, msg):
         data = numpy.array(msg.data).reshape(3, len(msg.data)//3, order='F')
         self.t = data[0, :].flatten()
         self.h = data[1:, :]
 
+
     def interpolate(self, t):
         h_fun = interp1d(self.t, self.h, **interpolate_config)
         return h_fun(t)
+
 
     def time_now(self):
         return self.t[-1]
