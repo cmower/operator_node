dde07dbfe50c7183a80c772bd5f4d548af63265b
Chris E. Mower <chris.mower@ed.ac.uk>
2021-09-01 16:12:14 +0100

    Add ParseMousePath class

--- src/operator_node/parser.py
+++ src/operator_node/parser.py
@@ -1,9 +1,11 @@
 import numpy
 from scipy.interpolate import interp1d
 
+"""
+TODO: the following could be abstracted
+"""
 
-class ParseInterfaceLog:
-
+class Parser:
 
     # https://docs.scipy.org/doc/scipy/reference/generated/scipy.interpolate.interp1d.html
     interpolate_config = dict(
@@ -11,6 +13,9 @@ class ParseInterfaceLog:
         bounds_error=False,
         fill_value='extrapolate'
     )
+
+
+class ParseInterfaceLog(Parser):
 
 
     def __init__(self, msg):
@@ -30,3 +35,35 @@ class ParseInterfaceLog:
 
     def h_now(self):
         return self.h[:, -1]
+
+
+class ParseMousePath(Parser)
+
+
+    def __init__(self, msg):
+
+        # Extract data
+        t = []
+        path = []
+        for p in msg.poses:
+            t.append(p.header.stamp.to_sec())
+            path.append([p.position.x, p.position.y])
+
+        # Parse as numpy arrays
+        self.t = numpy.array(t)
+        self.path = numpy.array(path).T  # 2-by-n
+
+        # Interpolate
+        self.path_fun = interp1d(self.t, self.path, **self.interpolate_config)
+
+
+    def interpolate(self, t):
+        return self.path_fun(t)
+
+
+    def t_now(self):
+        return self.t[-1]
+
+
+    def path_now(self):
+        return self.path[:, -1]
