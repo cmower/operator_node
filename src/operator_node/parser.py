da0c86c19c794d6be1a96435380e92f9c23470a8
Chris E. Mower <chris.mower@ed.ac.uk>
2022-01-26 18:11:22 +0000

    Update year

--- src/operator_node/parser.py
+++ src/operator_node/parser.py
@@ -1,5 +1,5 @@
 # BSD 2-Clause License
-# Copyright (c) 2021, Christopher E. Mower
+# Copyright (c) 2022, Christopher E. Mower
 # All rights reserved.
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
